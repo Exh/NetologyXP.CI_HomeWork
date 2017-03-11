6be343a8bf87a967376e4caa286e7d94b7874fa1
e.tarasenko <exh@xakep.ru>
2017-03-11 18:42:52 +0300

    add test I put two same "Harry Potter and the Philosopher\'s Stone" in cart. Total cost of my cart is 16 EUR

--- homework/HarryPotter/tests/book_shop_tests.js
+++ homework/HarryPotter/tests/book_shop_tests.js
@@ -14,4 +14,13 @@ suite('buy Harry Potter books', function () {
         assert.equal(res, 8)
     });
 
+    test('I put two same "Harry Potter and the Philosopher\'s Stone" in cart. Total cost of my cart is 16 EUR', function(){
+        bookshop = new BookShop();
+        bookshop.put("Harry Potter and the Philosopher's Stone");
+
+        res = bookshop.totalCost();
+
+        assert.equal(res, 16)
+    });
+
 });
