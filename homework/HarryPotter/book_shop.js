f5456e6ca5eff4c3f3cb149b8a4d4040e9561a61
e.tarasenko <exh@xakep.ru>
2017-03-11 18:53:43 +0300

    implement simple cart for bookshop fix totalCost evaluation

--- homework/HarryPotter/book_shop.js
+++ homework/HarryPotter/book_shop.js
@@ -2,14 +2,24 @@
 
 class BookShop
 {
+    constructor()
+    {
+        this._cart= [];
+    }
+
     put(book)
     {
-
+        if (book in this._cart)
+            this._cart[book] += 1;
+        else
+            this._cart[book] = 1
     }
 
     totalCost()
     {
-        return 8;
+        for (var key in this._cart) {
+            return 8 * this._cart[key];
+        }
     }
 }
 
